f845bbc927540ede036214001694be0afbf18c14
Th30s <43751800+Th30s@users.noreply.github.com>
2019-04-17 10:03:47 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(".theFuture").click(function(){
     var userInput1 = parseInt($(".oraora").val());
     var userInput2 = $(".zaWarudo").val().toLowerCase();
         if(userInput1 >= "10" && userInput2 === "yes"){
-            $(".results").html("<img src='https://i.kym-cdn.com/photos/images/newsfeed/000/754/539/566.gif'>");
+            $(".results").html("<a href ='https://www.youtube.com/watch?v=HaGkk60kcjQ' target='_blank'> <img src='https://i.kym-cdn.com/photos/images/newsfeed/000/754/539/566.gif'></a>");
         } else if (userInput1 < "10" && userInput2 === "yes"){
-            $(".results").html("<img src='https://thumbs.gfycat.com/DeficientSilentCero-max-1mb.gif'>");
+            $(".results").html("<a href='https://www.youtube.com/watch?v=VtzvlXL9gXk' target='_blank'><img src='https://thumbs.gfycat.com/DeficientSilentCero-max-1mb.gif'></a>");
         }else if (userInput1 >="10" && userInput2 === "no"){
-            $(".results").html("<img src='https://i.ytimg.com/vi/o5lj1Pgjruo/maxresdefault.jpg'>");
+            $(".results").html("<a href='https://www.youtube.com/watch?v=OzHE5q1NGa4' target='_blank'><img src='https://i.ytimg.com/vi/o5lj1Pgjruo/maxresdefault.jpg'></a>");
         }else if (userInput1 < "10" && userInput2 === "no"){
-            $(".results").html("<img src='https://rei.animecharactersdatabase.com/uploads/chars/5688-1505669513.jpg'>");
+            $(".results").html("<a href='https://www.youtube.com/watch?v=CxXrHXcG8WI' target='_blank'><img src='https://rei.animecharactersdatabase.com/uploads/chars/5688-1505669513.jpg'></a>");
         }
         
         
